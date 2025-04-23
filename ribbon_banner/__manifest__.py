d4e0fca77abf769d733021d251770bee2f73afd8
Edgardo Rios <edgardo.rios@gmail.com>
2025-04-23 12:50:12 -0600

    Fix duplicate ribbon banner in website editor by checking website_id context (version 18.0.2.0.0)

--- ribbon_banner/__manifest__.py
+++ ribbon_banner/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Ribbon Banner',
-    'version': '18.0.1.0.0',
+    'version': '18.0.2.0.0',
     'category': 'Tools',
     'summary': 'Add a customizable ribbon banner to display environment and database name',
     'description': """
